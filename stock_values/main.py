b642cebd53a366502e98070bcb22a7f0b53543db
Heikki <heikki.kurppa@awacademy.student>
2022-06-01 17:57:27 +0300

    run program from jupyter notebook

--- stock_values/main.py
+++ stock_values/main.py
@@ -2,11 +2,14 @@ import pandas as pd
 from datetime import datetime, timedelta
 from functions import *
 
+# Ticker and days variables imported from jupyter notebook
+
+
 DATE_TO = datetime.now()
-DATE_FROM = DATE_TO - timedelta(days=200)
+DATE_FROM = DATE_TO - timedelta(days=days)
 
 
-stock_data = get_stock_data("FORTUM.HE", DATE_FROM)
+stock_data = get_stock_data(ticker, DATE_FROM)
 stock_data = get_weekdays(stock_data)
 stock_data = get_diff(stock_data)
 weekday_avarage = group_by_weekday(stock_data)
@@ -15,5 +18,5 @@ stock_data = get_diff_from_max(stock_data)
 stock_data = get_diff_from_min(stock_data)
 stock_data = get_diff_percent_from_avg(stock_data)
 
-
-print(stock_data.tail(30))
+stock_data.to_csv("data/stock_data.csv", index=False)
+# print(stock_data.head(30))
